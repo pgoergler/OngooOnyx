cf224220402c1ff03d038b0f11f0f8a7d99b7d05
Paul GOERGLER <pgoergler@gmail.com>
2017-02-20 17:00:06 +0400

    Add OptionalObject

---
+++ src/Ongoo/Onyx/Utils/OptionalObject.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ongoo\Onyx\Utils;
+
+/**
+ * Description of OptionalObject
+ *
+ * @author paul
+ */
+class OptionalObject
+{
+    
+    protected $object;
+    protected $defaultValue = null;
+    
+    public static function of($object = null) {
+        return new OptionalObject($object);
+    }
+    
+    protected function __construct(&$object = null) {
+        $this->object = $object;
+    }
+    
+    public function defaultAs($defaultValue)
+    {
+        $this->defaultValue = $defaultValue;
+        return $this;
+    }
+    
+    public function __call($methodName, $args) {
+        if( is_null($this->object) )
+        {
+            return $this->defaultValue;
+        }
+        
+        return call_user_func_array(array($this->object, $methodName), $args);
+    }
+}
