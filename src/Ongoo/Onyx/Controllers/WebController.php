cac61e9d5d002e819c8b7cb37317762b941cf84e
Paul GOERGLER <pgoergler@gmail.com>
2017-12-14 11:42:16 +0400

    FIX: Add deprecated functions
    forwardXXX and forwardXXXUnless are deprecated

--- src/Ongoo/Onyx/Controllers/WebController.php
+++ src/Ongoo/Onyx/Controllers/WebController.php
@@ -84,4 +84,46 @@ class WebController
         return $this->app['request.abort_if']($condition, 401, $message);
     }
 
+    public function forwardCodeUnless($code, $condition, $message = "error")
+    {
+        trigger_error('forwardCodeUnless is deprecated', E_USER_DEPRECATED);
+        $this->abortUnless($condition, $code, $message);
+    }
+
+    public function forwardCode($code, $condition, $message = "error")
+    {
+        trigger_error('forwardCode is deprecated', E_USER_DEPRECATED);
+        $this->abortIf(!$condition, $code, $message);
+    }
+
+    public function forward404Unless($condition)
+    {
+        trigger_error('forward404Unless is deprecated', E_USER_DEPRECATED);
+        $this->abort404Unless($condition);
+    }
+
+    public function forward404($condition)
+    {
+        trigger_error('forward404 is deprecated', E_USER_DEPRECATED);
+        $this->abort404If($condition);
+    }
+
+    public function forward401Unless($condition)
+    {
+        trigger_error('forward401Unless is deprecated', E_USER_DEPRECATED);
+        $this->abort401Unless($condition);
+    }
+
+    public function forward401($condition)
+    {
+        trigger_error('forward401 is deprecated', E_USER_DEPRECATED);
+        $this->abort401If($condition);
+    }
+
+    public function execute($action = 'index', $args = array())
+    {
+        $fn = sprintf('execute%s', ucfirst($action));
+        return call_user_func_array(array($this, $fn), $args);
+    }
+
 }
